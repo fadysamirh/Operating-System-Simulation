9ef1fcfc2f878166cf32582e90996fda0b9e87e2
Fady <fadysamirh@gmail.com>
2020-04-26 15:17:05 +0200

    OS

---
+++ src/WriteSemaphore.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class WriteSemaphore {
+	Queue<Process> queue;
+	AtomicBoolean value;
+
+	public WriteSemaphore() {
+		// TODO Auto-generated constructor stub
+		queue = new LinkedList<Process>();
+		value = new AtomicBoolean();
+		value.set(true);
+	}
+
+	public void semWait(Process p) {
+		if (value.compareAndSet(true, false)) {
+			
+		} else {
+			queue.add(p);
+			p.setProcessState(p, ProcessState.Waiting);
+			p.isSuspended=true;
+			p.suspend();
+
+				
+		}
+		
+	}
+
+	public void semPost(Process p) {
+		if (queue.isEmpty()) {
+			value.set(true);
+		} else {
+			Process removed= queue.remove();
+			removed.setProcessState(removed, ProcessState.Ready);
+			OperatingSystem.readyQueue.add(removed);
+		}
+	}
+}
